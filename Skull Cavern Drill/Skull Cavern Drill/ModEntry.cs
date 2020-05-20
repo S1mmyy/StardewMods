c64887efd8e0285337137c3c6f2b6fd59f538dbd
S1mmyy <simmypoo385@gmail.com>
2020-05-20 15:47:22 -0400

    Changed structure so that the patch is now made inside the harmony class.
    Both the JA API and the drill ID now are gotten when a save is loaded.

--- Skull Cavern Drill/Skull Cavern Drill/ModEntry.cs
+++ Skull Cavern Drill/Skull Cavern Drill/ModEntry.cs
@@ -3,6 +3,7 @@ using StardewModdingAPI;
 using StardewValley;
 using StardewValley.Locations;
 using System;
+using StardewModdingAPI.Events;
 
 namespace Skull_Cavern_Drill
 {
@@ -15,13 +16,13 @@ namespace Skull_Cavern_Drill
     {
         public override void Entry(IModHelper helper)
         {
-            ObjectPatches.Initialize(helper, Monitor);
-            var harmony = HarmonyInstance.Create(this.ModManifest.UniqueID);
+            helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+            ObjectPatches.Apply(this.ModManifest.UniqueID);
+        }
 
-            harmony.Patch(
-            original: AccessTools.Method(typeof(StardewValley.Object), nameof(StardewValley.Object.placementAction)),
-            prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.PlacementAction_Prefix))
-            );
+        private void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            ObjectPatches.Initialize(Helper, Monitor);
         }
     }
 
@@ -29,26 +30,37 @@ namespace Skull_Cavern_Drill
     {
         private static IApi api;
         private static IMonitor Monitor;
+        private static int MyCraftableID = -1;
+
+        public static void Apply(string modId)
+        {
+            var harmony = HarmonyInstance.Create(modId);
+
+            harmony.Patch(
+            original: AccessTools.Method(typeof(StardewValley.Object), nameof(StardewValley.Object.placementAction)),
+            prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.PlacementAction_Prefix))
+            );
+        }
 
         public static void Initialize(IModHelper helper, IMonitor monitor)
         {
-            // api is never set to anything, as .GetApi can't be used, since JA isn't loaded.
-            // Figure out where/when I need to call Initialize() in Entry().
             api = helper.ModRegistry.GetApi<IApi>("spacechase0.JsonAssets");
             Monitor = monitor;
+
+            if (api != null)
+            {
+                MyCraftableID = api.GetBigCraftableId("Dwarven Drill");
+            }
         }
 
         internal static bool PlacementAction_Prefix(StardewValley.Object __instance, GameLocation location, int x, int y, ref bool __result, Farmer who = null)
         {
             try
             {
-                if (api != null)
+                if ((__instance.bigCraftable.Value && __instance.ParentSheetIndex == MyCraftableID) && (location is MineShaft shaft && Game1.CurrentMineLevel > 120))
                 {
-                    if ((location is MineShaft && Game1.CurrentMineLevel > 120) && __instance.ParentSheetIndex == api.GetBigCraftableId("Dwarven Drill"))
-                    {
-                        (location as MineShaft).createLadderDown(x, y, true);
-                        __result = true;
-                    }
+                    shaft.createLadderDown(x, y, true);
+                    __result = true;
                 }
                 return false;
             }
