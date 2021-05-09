ed2f7677c0d8cd42bfef4265267abd89de93d059
S1mmyy <simmypoo385@gmail.com>
2021-05-09 13:10:31 -0400

    Source code for AutoTrash mod

---
+++ AutoTrash/AutoTrash/ModConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using StardewModdingAPI;
+
+namespace AutoTrash
+{
+    class ModConfig
+    {
+        public bool MinesOnly { get; set; } = true;
+        public IList<int> DeleteItems { get; set; } = new List<int>();
+        public SButton ToggleTrash { get; set; } = SButton.RightAlt;
+    }
+}
